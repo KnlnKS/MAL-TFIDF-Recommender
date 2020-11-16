7003a39d290301a8bc7c9f72969adcbf6fc39b18
KnlnKS <soulkks@hotmail.ca>
2020-11-16 00:06:48 -0500

    Update DataHandler.py
    Started generating genre ids instead of using plain text.

--- DataHandler.py
+++ DataHandler.py
@@ -4,14 +4,16 @@ pd.set_option('display.max_columns', None)
 class DataHandler():
     def __init__(self, is_limited):
         self._load_data(is_limited)
-        self._remove_nans_from_genres()
+        self._generate_genre_ids()
 
-
-    def _remove_nans(self, s):
+    def _string_to_list(self, s):
         if isinstance(s, str):
-            return s
+            return s.split(',')
         else:
             return 'none'
+
+    def _list_to_string(self, a):
+            return str(a)[1:-1]
 
     def _load_data(self, is_limited):
         self.anime = pd.read_csv('./input/anime_cleaned.csv')[['anime_id', 'title', 'score', 'genre']].sort_values(by='score', ascending=False)
@@ -23,8 +25,12 @@ class DataHandler():
                 ['username', 'anime_id', 'my_score']]
 
 
-    def _remove_nans_from_genres(self):
-        self.anime['genre'] = self.anime['genre'].apply(self._remove_nans)
+    def _generate_genre_ids(self):
+        self.anime['genre'] = self.anime['genre'].apply(self._string_to_list)
+        v = self.anime['genre'].explode()
+        v[:] = pd.factorize(v)[0] + 1
+        self.anime['genre'] = v.groupby(level=0).agg(list)
+        self.anime['genre'] = self.anime['genre'].apply(self._list_to_string)
 
 
 
