1a937a8146e5e85547e8f1885016facf92c3a0a3
KnlnKS <soulkks@hotmail.ca>
2020-11-16 14:12:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,9 +11,22 @@ dataHandler = d.DataHandler(False)
 print('Data loaded and preprocessed in ' + str(t.time() - start) + ' seconds.')
 print()
 
+# Use TFIDF algo
+start = t.time()
 similarities = tf.TFIDF(dataHandler.anime)
+print('Genre importance calculated in ' + str(t.time() - start) + ' seconds.')
+print()
+
+# Get recommendations
+start = t.time()
 recommender = u.UserProfiler(similarities, dataHandler)
-#print(recommender.get_user_ratings('Kvazikvark').sort_values(by='my_score', ascending=False))
-m = recommender.get_user_recommendations('Kvazikvark')
-print(m.size)
-print(m.head())
+print('Recommednations generated in ' + str(t.time() - start) + ' seconds.')
+print()
+
+watched_anime = recommender.get_user_ratings('user in animelist_cleaned.csv').sort_values(by='my_score', ascending=False)
+recommendations = recommender.get_user_recommendations('user in animelist_cleaned.csv')
+
+print('User has watched '+str(watched_anime.size)+' anime.')
+print('Fetched '+str(recommendations.size)+' recommendations.')
+print()
+print(recommendations.head())
